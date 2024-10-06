b5c2f53d0a6f2a8a9a011e32bb01cddd9a01114d
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2024-10-06 21:45:57 +0530

    convet mem.c to a STB style header file

--- src/utils/mem.h
+++ src/utils/mem.h
@@ -8,3 +8,28 @@ void *malloc_with_check(size_t size);
 void *realloc_with_check(void *pre, size_t size);
 
 #endif
+
+#ifdef _LOOOKA_MEM_IMPLEMENTATION
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_with_check(size_t size) {
+    void *mem = malloc(size);
+    if (mem == NULL) {
+        perror("Cannot allocated memory");
+        exit(EXIT_FAILURE);
+    }
+    return mem;
+}
+
+void *realloc_with_check(void *ptr, size_t size) {
+    void *mem = realloc(ptr, size);
+    if (mem == NULL) {
+        perror("Cannot reallocate memory");
+        exit(EXIT_FAILURE);
+    }
+    return mem;
+}
+
+#endif
